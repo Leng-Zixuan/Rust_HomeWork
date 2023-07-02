b9ea5339d4adbd6ce3ff5ce007b7597c03a307e6
金宇轩 <jinyuxuan@co-mall.com>
2023-07-02 21:12:14 +0800

    rust 第一课

--- lesson_01/target/debug/deps/lesson_01-cca32d6e0d435db6.d
+++
@@ -1,5 +0,0 @@
-/Users/jinyuxuan/work/demo/Rust_HomeWork/lesson_01/target/debug/deps/lesson_01-cca32d6e0d435db6: src/main.rs
-
-/Users/jinyuxuan/work/demo/Rust_HomeWork/lesson_01/target/debug/deps/lesson_01-cca32d6e0d435db6.d: src/main.rs
-
-src/main.rs:
--- lesson_01/target/debug/lesson_01.d
+++
@@ -1,1 +0,0 @@
-/Users/jinyuxuan/work/demo/Rust_HomeWork/lesson_01/target/debug/lesson_01: /Users/jinyuxuan/work/demo/Rust_HomeWork/lesson_01/src/main.rs
